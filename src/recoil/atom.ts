414c9303c43deec89232505f38cf143affd2f7aa
jwchoi-alticast <54067227+CJW-project@users.noreply.github.com>
2024-02-14 10:38:53 +0900

    feat : recoil persist 적용
    - localStorage, sessionStorage 저장 시 사용

--- src/recoil/atom.ts
+++ src/recoil/atom.ts
@@ -1,8 +1,18 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist(); // default: localstorage
+
+// sessionStorage 사용 시
+// const { persistAtom } = recoilPersist({
+//   key: 'sessionStorage',
+//   storage: sessionStorage,
+// })
 
 export const countState = atom({
   key: "countState",
   default: 0,
+  effects_UNSTABLE: [persistAtom]
 });
 
 export const todoListState = atom({
@@ -14,9 +24,11 @@ export const todoListState = atom({
     { contents: "content4", status: "HOLD" },
     { contents: "content5", status: "DOING" },
   ],
+  effects_UNSTABLE: [persistAtom]
 });
 
 export const textState = atom({
   key: "textState",
   default: { status: "" },
+  effects_UNSTABLE: [persistAtom]
 });
